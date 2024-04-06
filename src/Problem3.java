c23e6a8f89fbdd52e1dce46960215877f523d6f9
Nurislam <154406475+MminkaA@users.noreply.github.com>
2024-04-06 23:21:42 +0500

    Update Problem3.java

--- src/Problem3.java
+++ src/Problem3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Problem3 {
-    public static boolean isPrime(int n) {
+    public static boolean isPrime(int n) { // complexity O(sqrt(n))
         if (n <= 1) return false;
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) return false;
